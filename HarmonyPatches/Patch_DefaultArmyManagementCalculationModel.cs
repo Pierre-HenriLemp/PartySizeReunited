748404bf610648ce7ecad42d9d5f50f220ffe5e1
P-H Lemp <pierrehenrilempereur1997@gmail.com>
2025-11-30 20:35:22 +0100

    Fix bug of double view in McM menu

--- HarmonyPatches/Patch_DefaultArmyManagementCalculationModel.cs
+++ HarmonyPatches/Patch_DefaultArmyManagementCalculationModel.cs
@@ -3,13 +3,13 @@ using TaleWorlds.CampaignSystem.GameComponents;
 
 namespace PartySizeReunited.HarmonyPatches
 {
-	[HarmonyPatch(typeof(DefaultArmyManagementCalculationModel), nameof(DefaultArmyManagementCalculationModel.CalculatePartyInfluenceCost))]
-	class Patch_DefaultArmyManagementCalculationModel
-	{
-		static void Postfix(ref int __result)
-		{
-			// Multiply result of CalculatePartyInfluenceCost that return the cost of influence by the setting PartyInfluenceCost that is a float percentage
-			__result = (int)(MCMUISettings.Instance.PartyInfluenceCost * __result);
-		}
-	}
+    [HarmonyPatch(typeof(DefaultArmyManagementCalculationModel), nameof(DefaultArmyManagementCalculationModel.CalculatePartyInfluenceCost))]
+    class Patch_DefaultArmyManagementCalculationModel
+    {
+        static void Postfix(ref int __result)
+        {
+            // Multiply result of CalculatePartyInfluenceCost that return the cost of influence by the setting PartyInfluenceCost that is a float percentage
+            __result = (int)(MCMUISettings.Instance?.PartyInfluenceCost ?? 1 * __result);
+        }
+    }
 }
--- PartySize.cs
+++ PartySize.cs
@@ -9,132 +9,132 @@ using TaleWorlds.Localization;
 
 namespace PartySizeReunited
 {
-	public class PartySize : DefaultPartySizeLimitModel
-	{
-		private readonly List<DefaultPartySizeLimitModel> _partySizeModels;
+    public class PartySize : DefaultPartySizeLimitModel
+    {
+        private readonly List<DefaultPartySizeLimitModel> _partySizeModels;
 
-		public PartySize(List<DefaultPartySizeLimitModel> partySizeModels = null)
-		{
-			_partySizeModels = partySizeModels ?? new List<DefaultPartySizeLimitModel>();
-		}
+        public PartySize(List<DefaultPartySizeLimitModel> partySizeModels)
+        {
+            _partySizeModels = partySizeModels ?? new List<DefaultPartySizeLimitModel>();
+        }
 
-		public override ExplainedNumber GetPartyMemberSizeLimit(PartyBase party, bool includeDescriptions = false)
-		{
-			ExplainedNumber partySize = base.GetPartyMemberSizeLimit(party, includeDescriptions);
+        public override ExplainedNumber GetPartyMemberSizeLimit(PartyBase party, bool includeDescriptions = false)
+        {
+            ExplainedNumber partySize = base.GetPartyMemberSizeLimit(party, includeDescriptions);
 
-			// Appliquer tous les modèles de taille découverts
-			foreach (var model in _partySizeModels)
-			{
-				try
-				{
-					Console.WriteLine($"Applying party size model from {model.GetType().Name}");
-					partySize = model.GetPartyMemberSizeLimit(party, includeDescriptions);
-				}
-				catch (Exception ex)
-				{
-					Console.WriteLine($"Error applying model {model.GetType().Name}: {ex.Message}");
-				}
-			}
+            // Appliquer tous les modèles de taille découverts
+            foreach (var model in _partySizeModels)
+            {
+                try
+                {
+                    Console.WriteLine($"Applying party size model from {model.GetType().Name}");
+                    partySize = model.GetPartyMemberSizeLimit(party, includeDescriptions);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error applying model {model.GetType().Name}: {ex.Message}");
+                }
+            }
 
-			// Appliquer le bonus de PartySizeReunited
-			ExplainedNumber finalPartySize = GetPartySizeUpdatedByPartySizeMod(party, partySize);
-			return finalPartySize;
-		}
+            // Appliquer le bonus de PartySizeReunited
+            ExplainedNumber finalPartySize = GetPartySizeUpdatedByPartySizeMod(party, partySize);
+            return finalPartySize;
+        }
 
-		private ExplainedNumber GetPartySizeUpdatedByPartySizeMod(PartyBase party, ExplainedNumber basePartySize)
-		{
-			Dropdown<ScopeExtension> bonusScope = MCMUISettings.Instance.BonusScope;
-			IScope selectedScope = bonusScope.SelectedValue.Scope;
-			float bonusPercentage = MCMUISettings.Instance.PartyBonusAmnt;
-			bool isPlayerImpacted = MCMUISettings.Instance.IsPlayerPartyImpacted;
-			TextObject partySizeBonusText = new TextObject("Party Size Reunited modifier");
+        private ExplainedNumber GetPartySizeUpdatedByPartySizeMod(PartyBase party, ExplainedNumber basePartySize)
+        {
+            Dropdown<ScopeExtension> bonusScope = MCMUISettings.Instance.BonusScope;
+            IScope selectedScope = bonusScope.SelectedValue.Scope;
+            float bonusPercentage = MCMUISettings.Instance.PartyBonusAmnt;
+            bool isPlayerImpacted = MCMUISettings.Instance.IsPlayerPartyImpacted;
+            TextObject partySizeBonusText = new TextObject("Party Size Reunited modifier");
 
-			ExplainedNumber result = basePartySize;
-			float newValue = (float)Math.Round(result.ResultNumber * bonusPercentage);
-			float valueToApply = newValue - result.ResultNumber;
-			if (party.LeaderHero != null)
-			{
-				switch (selectedScope)
-				{
-					case IScope.Everyone:
-						if (!party.LeaderHero.IsHumanPlayerCharacter)
-						{
-							result.Add(valueToApply, partySizeBonusText);
-							SetNoMoreSupplyNeeded(party);
-						}
-						break;
+            ExplainedNumber result = basePartySize;
+            float newValue = (float)Math.Round(result.ResultNumber * bonusPercentage);
+            float valueToApply = newValue - result.ResultNumber;
+            if (party.LeaderHero != null)
+            {
+                switch (selectedScope)
+                {
+                    case IScope.Everyone:
+                        if (!party.LeaderHero.IsHumanPlayerCharacter)
+                        {
+                            result.Add(valueToApply, partySizeBonusText);
+                            SetNoMoreSupplyNeeded(party);
+                        }
+                        break;
 
-					case IScope.Only_player_clan:
-						if (!party.LeaderHero.IsHumanPlayerCharacter &&
-							party.LeaderHero.Clan != null &&
-							Hero.MainHero?.Clan != null &&
-							party.LeaderHero.Clan == Hero.MainHero.Clan)
-						{
-							result.Add(valueToApply, partySizeBonusText);
-							SetNoMoreSupplyNeeded(party);
-						}
-						break;
+                    case IScope.Only_player_clan:
+                        if (!party.LeaderHero.IsHumanPlayerCharacter &&
+                            party.LeaderHero.Clan != null &&
+                            Hero.MainHero?.Clan != null &&
+                            party.LeaderHero.Clan == Hero.MainHero.Clan)
+                        {
+                            result.Add(valueToApply, partySizeBonusText);
+                            SetNoMoreSupplyNeeded(party);
+                        }
+                        break;
 
-					case IScope.Only_player_faction:
-						if (!party.LeaderHero.IsHumanPlayerCharacter &&
-							party.LeaderHero.Clan?.MapFaction != null &&
-							Hero.MainHero?.Clan?.MapFaction != null &&
-							party.LeaderHero.Clan.MapFaction.Name == Hero.MainHero.Clan.MapFaction.Name)
-						{
-							result.Add(valueToApply, partySizeBonusText);
-							SetNoMoreSupplyNeeded(party);
-						}
-						break;
+                    case IScope.Only_player_faction:
+                        if (!party.LeaderHero.IsHumanPlayerCharacter &&
+                            party.LeaderHero.Clan?.MapFaction != null &&
+                            Hero.MainHero?.Clan?.MapFaction != null &&
+                            party.LeaderHero.Clan.MapFaction.Name == Hero.MainHero.Clan.MapFaction.Name)
+                        {
+                            result.Add(valueToApply, partySizeBonusText);
+                            SetNoMoreSupplyNeeded(party);
+                        }
+                        break;
 
-					case IScope.Only_ennemies:
-						if (!party.LeaderHero.IsHumanPlayerCharacter &&
-							party.LeaderHero.Clan?.MapFaction != null &&
-							Hero.MainHero?.Clan?.MapFaction != null &&
-							party.LeaderHero.Clan.MapFaction.Name != Hero.MainHero.Clan.MapFaction.Name)
-						{
-							result.Add(valueToApply, partySizeBonusText);
-							SetNoMoreSupplyNeeded(party);
-						}
-						break;
-				}
+                    case IScope.Only_ennemies:
+                        if (!party.LeaderHero.IsHumanPlayerCharacter &&
+                            party.LeaderHero.Clan?.MapFaction != null &&
+                            Hero.MainHero?.Clan?.MapFaction != null &&
+                            party.LeaderHero.Clan.MapFaction.Name != Hero.MainHero.Clan.MapFaction.Name)
+                        {
+                            result.Add(valueToApply, partySizeBonusText);
+                            SetNoMoreSupplyNeeded(party);
+                        }
+                        break;
+                }
 
-				if (isPlayerImpacted && party.LeaderHero.IsHumanPlayerCharacter == true)
-				{
-					// Update player's party
-					result.Add(valueToApply, partySizeBonusText);
-				}
-			}
+                if (isPlayerImpacted && party.LeaderHero.IsHumanPlayerCharacter == true)
+                {
+                    // Update player's party
+                    result.Add(valueToApply, partySizeBonusText);
+                }
+            }
 
-			return result;
-		}
+            return result;
+        }
 
-		private void SetNoMoreSupplyNeeded(PartyBase party)
-		{
-			bool noMoreSupplyNeeded = MCMUISettings.Instance.NoMoreSupplyIssues;
-			if (noMoreSupplyNeeded)
-			{
-				SetGoldBonus(party);
-				SetFoodBonus(party);
-			}
+        private void SetNoMoreSupplyNeeded(PartyBase party)
+        {
+            bool noMoreSupplyNeeded = MCMUISettings.Instance.NoMoreSupplyIssues;
+            if (noMoreSupplyNeeded)
+            {
+                SetGoldBonus(party);
+                SetFoodBonus(party);
+            }
 
-		}
+        }
 
-		private void SetGoldBonus(PartyBase party)
-		{
-			if (party.LeaderHero.Gold < party.MobileParty.TotalWage)
-			{
-				int bonus = party.MobileParty.TotalWage * 2;
-				party.LeaderHero.Gold += bonus;
-			}
-		}
+        private void SetGoldBonus(PartyBase party)
+        {
+            if (party.LeaderHero.Gold < party.MobileParty.TotalWage)
+            {
+                int bonus = party.MobileParty.TotalWage * 2;
+                party.LeaderHero.Gold += bonus;
+            }
+        }
 
-		private void SetFoodBonus(PartyBase party)
-		{
-			if (party.RemainingFoodPercentage < 1000)
-			{
-				int bonus = party.NumberOfAllMembers > 200 ? party.NumberOfAllMembers * 2 : 200;
-				party.RemainingFoodPercentage += bonus * 10;
-			}
-		}
-	}
+        private void SetFoodBonus(PartyBase party)
+        {
+            if (party.RemainingFoodPercentage < 1000)
+            {
+                int bonus = party.NumberOfAllMembers > 200 ? party.NumberOfAllMembers * 2 : 200;
+                party.RemainingFoodPercentage += bonus * 10;
+            }
+        }
+    }
 }
